7c5285b47a080bab6187f9232c0178df8d228405
49512 - Nuno Aguiar <a49512@alunos.isel.pt>
2025-07-02 15:31:15 +0100

    Frontend: Changed: - Continuation of the last commit

--- frontend/services/fetchUniversities.tsx
+++ frontend/services/fetchUniversities.tsx
@@ -3,6 +3,14 @@ import {apiUrl} from "./fetchWelcome";
 import {UniversityInterface} from "../types/UniversityInterface";
 import {handleAxiosError} from "../utils/HandleAxiosError";
 
+
+
+/**
+ * Fetches a list of universities based on a search query.
+ * @param {string} searchQuery - The search term to filter universities by name or description.
+ * @returns {Promise<UniversityInterface[]>} - Returns a promise that resolves to an array of university objects.
+ * @throws {Error} - Throws an error if the request fails.
+ */
 export const fetchUniversities = async (searchQuery: string): Promise<UniversityInterface[]> => {
     try {
         const response = await axios.get(
--- frontend/services/fetchWelcome.tsx
+++ frontend/services/fetchWelcome.tsx
@@ -3,6 +3,15 @@ import {handleAxiosError} from "../utils/HandleAxiosError";
 import {WelcomeData} from "../types/welcome/WelcomeInterfaces";
 import {isMobile} from "../utils/DeviceType";
 
+
+/**
+ * Fetches welcome data from the server.
+ * This function constructs the API URL based on the device type (mobile or web)
+ * and retrieves welcome data from the specified endpoint.
+ *
+ * @returns {Promise<WelcomeData>} - A promise that resolves to the welcome data.
+ * @throws {Error} - Throws an error if the request fails.
+ */
 export const apiUrl = !isMobile
     ? 'https://localhost/api/'
     : 'http://10.0.2.2:8080/api/';
@@ -12,7 +21,6 @@ export const fetchWelcome = async (): Promise<WelcomeData> => {
         const response = await axios.get(apiUrl);
         return response.data;
     } catch (error) {
-        console.log(error);
         throw handleAxiosError(error);
     }
 };
